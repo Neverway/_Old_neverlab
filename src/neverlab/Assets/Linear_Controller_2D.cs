0e1a10e92176a141576e403dcc963342c86cddcc
Brian Sheldon <brianmsheldon@gmail.com>
2022-11-24 18:19:20 -0500

    Perform axis inversion in fixed update

--- src/neverlab/Assets/Linear_Controller_2D.cs
+++ src/neverlab/Assets/Linear_Controller_2D.cs
@@ -38,16 +38,14 @@ public class Linear_Controller_2D : MonoBehaviour
 
     private void FixedUpdate()
     {
+	    // Invert horizontal axis
+	    if (invertHorizontalAxis) movement.x *= -1;
+	    
+	    // Invert vertical axis
+	    if (invertVerticalAxis) movement.y *= -1;
+
 	    // Update object position
 	    transform.Translate(movement * moveSpeed * Time.deltaTime);
-	    
-	    // Set horizontal inverted inputs
-	    if (invertHorizontalAxis) horizontalInversion = -1;
-	    else horizontalInversion = 1;
-	    
-	    // Set vertical inverted inputs
-	    if (invertVerticalAxis) verticalInversion = -1;
-	    else verticalInversion = 1;
     }
     
     //=-----------------=
@@ -56,13 +54,13 @@ public class Linear_Controller_2D : MonoBehaviour
     private void UserInput()
     {
 	    // Horizontal
-	    if (Input.GetButton("MoveLeft")) movement.x = -1 * horizontalInversion;
-	    else if (Input.GetButton("MoveRight")) movement.x = 1 * horizontalInversion;
+	    if (Input.GetButton("MoveLeft")) movement.x = -1;
+	    else if (Input.GetButton("MoveRight")) movement.x = 1;
 	    else movement.x = 0;
 	    
 	    // Vertical
-	    if (Input.GetButton("MoveUp")) movement.y = 1 * verticalInversion;
-	    else if (Input.GetButton("MoveDown")) movement.y = -1 * verticalInversion;
+	    if (Input.GetButton("MoveUp")) movement.y = 1;
+	    else if (Input.GetButton("MoveDown")) movement.y = -1;
 	    else movement.y = 0;
     }
 
